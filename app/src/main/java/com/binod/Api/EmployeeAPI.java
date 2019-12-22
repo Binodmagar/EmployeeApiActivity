db1673fa267891c7bf8acac535fd763bbb507776
Binodmagar <ranabinod615@gmail.com>
2019-12-22 08:06:56 +0545

    creating Api packages and create EmployeeAPI interface

--- app/src/main/java/com/binod/Api/EmployeeAPI.java
+++ app/src/main/java/com/binod/Api/EmployeeAPI.java
@@ -1,4 +1,14 @@
 package com.binod.Api;
 
+import com.binod.Model.Employee;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 public interface EmployeeAPI {
+
+    @GET("employees")
+    Call<List<Employee>> getAllEmployee();
 }
