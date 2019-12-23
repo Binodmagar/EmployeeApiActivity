0399ae51c41c3c025e480823223006c719d4d61c
Binodmagar <ranabinod615@gmail.com>
2019-12-23 19:25:52 +0545

    Register Employee done

--- app/src/main/java/com/binod/Api/EmployeeAPI.java
+++ app/src/main/java/com/binod/Api/EmployeeAPI.java
@@ -1,17 +1,24 @@
 package com.binod.Api;
 
 import com.binod.Model.Employee;
+import com.binod.Model.EmployeeCUD;
 
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Path;
 
 public interface EmployeeAPI {
 
     @GET("employees")
     Call<List<Employee>> getAllEmployee();
+
+
+    @POST("create")
+    Call<Void> registerEmployee(@Body EmployeeCUD emp);
 
 
     @GET("employees/{empID}")
--- app/src/main/java/com/binod/employeeapiactivity/RegisterActivity.java
+++ app/src/main/java/com/binod/employeeapiactivity/RegisterActivity.java
@@ -3,12 +3,71 @@ package com.binod.employeeapiactivity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.binod.Api.EmployeeAPI;
+import com.binod.Model.EmployeeCUD;
+import com.binod.URL.URL;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    EditText etName, etSalary, etAge;
+    Button btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        etName = findViewById(R.id.etName);
+        etSalary = findViewById(R.id.etSalary);
+        etAge = findViewById(R.id.etAge);
+        btnRegister = findViewById(R.id.btnRegister);
+
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Register();
+            }
+        });
+    }
+
+    private void Register(){
+        String name = etName.getText().toString();
+        Float salary = Float.parseFloat(etSalary.getText().toString());
+        int age = Integer.parseInt(etAge.getText().toString());
+
+        EmployeeCUD employeeCUD = new EmployeeCUD(name, salary, age);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(URL.base_url)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        EmployeeAPI employeeAPI = retrofit.create(EmployeeAPI.class);
+        Call<Void> voidCall = employeeAPI.registerEmployee(employeeCUD);
+
+        voidCall.enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                Toast.makeText(RegisterActivity.this, " You are registered successfully!", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+
+                Toast.makeText(RegisterActivity.this, "Errors" + t.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
